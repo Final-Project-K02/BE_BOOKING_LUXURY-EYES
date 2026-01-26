59cf604dcc0ee37bf60beb2eec39f6c73e526707
NHL-PH50779 <lamnhph50779@gmail.com>
2026-01-26 10:28:14 +0700

    schedules-manager

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import doctorRouter from "../modules/doctor/doctor.routes.js";
+import scheduleRouter from "../modules/schedule/schedule.routes.js";
 
 const router = Router()
 router.use("/doctors", doctorRouter)
+router.use("/schedules", scheduleRouter)
+
 export default router;
